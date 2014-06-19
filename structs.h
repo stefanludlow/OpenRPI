88dfcb9552c2c94cd71c5c91ae12477abde4caf5
Thomas Paige <tpaige99@gmail.com>
2014-06-19 07:30:35 +0000

    Update rollcall to show name

--- structs.h
+++ structs.h
@@ -4162,7 +4162,7 @@ struct encumberance_info
 #define MOON_CYCLE 2881443 // 29 days, 12 hours, 44 minutes and 3 seconds)
 #define MOON_ORBIT 2388720 // 27.3days
 
-#define ACTIVITY_TIMER_MAX 172800
+#define ACTIVITY_TIMER_MAX 86400
 
 
 #define MODE_COMMAND		(1 << 0)
