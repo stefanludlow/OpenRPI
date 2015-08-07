054ad2c9de451b94de8fc42f1464f4f3a554d788
Icarus Local <ludlow.stefan@gmail.com>
2015-08-07 00:46:49 -0400

    Updated the script again. Notes to remember: have to chmod the script.

--- olc.cpp
+++ olc.cpp
@@ -2069,7 +2069,7 @@ do_zsave (CHAR_DATA * ch, char *arg, int cmd)
         }
 		if (engine.in_build_mode())
 		{
-			system("/home/sanctuary/bp/scripts/backup");
+			system("sh /home/sanctuary/bp/scripts/backup.sh");
 		}
         save_mob_progs();
         save_obj_progs();
